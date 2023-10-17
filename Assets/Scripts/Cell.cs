691e566e3ebf95cf20b2de1f859800490f566c23
Craig Franceschetti <cfranceschetti33@rrc.ca>
2023-10-17 16:16:46 -0500

    got the board working finally. The board correctly fills, and the cells correctly grab their neighbors if they have one. Now I need to implement the rules of the game of life.

--- Assets/Scripts/Cell.cs
+++ Assets/Scripts/Cell.cs
@@ -5,9 +5,19 @@ using UnityEngine;
 public class Cell : MonoBehaviour
 {
     private SpriteRenderer spriteRenderer;
+    public Cell[] cellNeighbors;
 
     public void Awake()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
+
+       
     }
+
+    private void Start()
+    {
+   
+    }
+
+
 }
--- Assets/Scripts/GameBoard.cs
+++ Assets/Scripts/GameBoard.cs
@@ -12,6 +12,7 @@ public class GameBoard : MonoBehaviour
 
     public Cell[][] board;
     public Cell cell;
+    
 
     private void Awake()
     {
@@ -30,8 +31,55 @@ public class GameBoard : MonoBehaviour
             for (int j = 0; j< board[i].Length; j++)
             {
                 board[i][j] = Instantiate<Cell>(cell, new Vector3(i, j, 0f), Quaternion.identity);
+                
             }
         }
+        //Board has been created. Now, we need to loop through this array again and find out how many neighbors they have.
+
+        
+        for (int i = 0; i < board.Length; i++)
+        {
+            for (int j = 0; j < board[i].Length; j++)
+            {
+                //Create neighbors array.
+                board[i][j].cellNeighbors = new Cell[8];
+
+                //fill neighbors array with all neighbors.
+                if (i - 1 >= 0 && j + 1 <= arraySize - 1)
+                {
+                    board[i][j].cellNeighbors[0] = board[i - 1][j + 1]; //top left
+                }
+                if (j + 1 <= arraySize - 1)
+                {
+                    board[i][j].cellNeighbors[1] = board[i][j + 1];  //top
+                }
+                if (i + 1 <= arraySize - 1 && j + 1 <= arraySize - 1)
+                {
+                    board[i][j].cellNeighbors[2] = board[i + 1][j + 1]; //top right
+                }
+                if (i + 1 <= arraySize - 1)
+                {
+                    board[i][j].cellNeighbors[3] = board[i + 1][j];     //right
+                }
+                if (i + 1 <= arraySize - 1 && j - 1 >= 0)
+                {
+                    board[i][j].cellNeighbors[4] = board[i + 1][j - 1]; //bottom right
+                }
+                if (j - 1 >= 0)
+                {
+                    board[i][j].cellNeighbors[5] = board[i][j - 1];     //bottom
+                }
+                if (i - 1 >= 0 && j - 1 >= 0)
+                {
+                    board[i][j].cellNeighbors[6] = board[i - 1][j - 1]; //bottom left
+                }
+                if (i - 1 >= 0)
+                {
+                    board[i][j].cellNeighbors[7] = board[i - 1][j];     //left
+                }
+            }
+        }
+
     }
 
     public void GameBoardUpdate()

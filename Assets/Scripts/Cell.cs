1ed04ff24ac296e64e8305e43f3ce172287a7fa7
Craig Franceschetti <cfranceschetti33@rrc.ca>
2023-10-25 15:39:31 -0500

    Refactored and commented all code.

--- Assets/Scripts/Cell.cs
+++ Assets/Scripts/Cell.cs
@@ -4,30 +4,43 @@ using UnityEngine;
 
 public class Cell : MonoBehaviour
 {
+    //Cell SpriteRenderer.
     private SpriteRenderer cellSpriteRenderer;
-    public Cell[] cellNeighbors;
-    public CellState cellState;
-    public int liveNeighbourCount = 0;
-    public int deadNeighbourCount = 0;
+    //Array of neighbouring cells.
+    private Cell[] cellNeighbours;
+    //Alive or dead state of cell.
+    private CellState cellState;
+    //How many live neighbours does this cell have
+    private int liveNeighbourCount = 0;
+    //How many dead neighbours does this cell have
+    private int deadNeighbourCount = 0;
 
     public void Awake()
     {
         cellSpriteRenderer = GetComponent<SpriteRenderer>();
     }
-
-    private void Start()
-    {
-
-    }
-
-    public void SetCellColor(Color color) { this.cellSpriteRenderer.color = color; }
-    public CellState GetCellState() {  return cellState; }
+    //Get the state of the cell.
+    public CellState GetCellState() { return cellState; }
+    //Get our cell neighbour array.
+    public Cell[] GetCellNeighbours() { return cellNeighbours; }
+    //Get the number of live neighbours this cell has.
+    public int GetLiveNeighbourCount() { return liveNeighbourCount; }
+    //Set the state of this cell.
     public void SetCellState(CellState cellState) { this.cellState = cellState; }
-
-    public Color GetSpriteRenderer() { return cellSpriteRenderer.color;}
+    //Setup our cell neighbour array.
+    public void SetCellNeighbours(Cell[] neighbours) { cellNeighbours = neighbours; }
+    //Set the colour of this cell.
+    public void SetCellColor(Color color) { this.cellSpriteRenderer.color = color; } 
+    //Add live cell neighbours
+    public void AddLiveNeighbourCount(int count) {  liveNeighbourCount += count; }
+    //Add dead cell neighbours
+    public void AddDeadNeighbourCount(int count) {  deadNeighbourCount += count; }
+    //Reset cell neighbour count.
+    public void ResetNeigbourCounts() { liveNeighbourCount = 0; 
+                                        deadNeighbourCount = 0; }
 
 }
-
+//Enum that keeps track of the cell's state of life.
 public enum CellState
 {
     Dead,
--- Assets/Scripts/GameBoard.cs
+++ Assets/Scripts/GameBoard.cs
@@ -1,44 +1,47 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Runtime.InteropServices.WindowsRuntime;
 using TMPro;
-using Unity.Mathematics;
 using UnityEngine;
+
 
 public class GameBoard : MonoBehaviour
 {
+    //Size of the array
     public int arraySize = 25;
+    //Camera
     public Camera cam;
-    public float boardTimer = 0f;
+    //Generation number that is on the UI.
     public TextMeshProUGUI generationNumber;
+    //How I count the generation internally.
     public int generationCounter = 1;
-    
+    //The text box for array size.
+    public TMP_InputField arraySizeUI;
+    //2D array of cells representing the current generation.
     public Cell[][] board;
+    //2D array of cells representing the next generation.
     public Cell[][] futureGen;
+    //Represents one cell.
     public Cell cell;
-    public float timer = 0f;
+    //Control the flow when we are paused.
     public bool paused = false;
 
     private void Awake()
     {
+        //Set our target frame rate to 4 FPS.
         Application.targetFrameRate = 4;
         SetupCamera();
+        //Generate our game boards based on array size.
         board = new Cell[arraySize][];
         futureGen = new Cell[arraySize][];
         CreateInitialGrid();
-        CreateNextGeneration();
-        // Board has been created. Now, we need to loop through this array again and find out how many neighbors they have.
-        // Fill Cell Neighbors
-        FillCellNeighbors();
-        // Need to check the rules of life first on the seed before moving to the next generation.
-        // Loop through board and grab the neighbours in the cell. 
-        // Loop through the neighbours and grab their enum status.
-        // If the neighbours are dead, consider making them null to make the determination much much easier. 
+        CreateNextGeneration();   
+        FillCellNeighbors();        
         CountNeighbours();
         RulesOfLife();
     }
 
+    //Updates the state of our game board for the next generation.
     private void UpdateStateOfBoard()
     {
         generationCounter++;
@@ -52,8 +55,14 @@ public class GameBoard : MonoBehaviour
         }
     }
 
+    //Fill our next generation with our current generation cells.
     private void CreateNextGeneration()
     {
+        //if (!arraySizeUI.text.Equals(String.Empty))
+        //{
+        //    arraySize = Int32.Parse(arraySizeUI.text);
+        //}
+
         //Create new board based on the current generation board to put the calculation results into.
         for (int i = 0; i < board.Length; i++)
         {
@@ -62,12 +71,12 @@ public class GameBoard : MonoBehaviour
             for (int j = 0; j < board[i].Length; j++)
             {
                 futureGen[i][j] = board[i][j];
-                board[i][j].liveNeighbourCount = 0;
-                board[i][j].deadNeighbourCount = 0;
+                board[i][j].ResetNeigbourCounts();
             }
         }
     }
 
+    //Performs the rules of life calculations.
     private void RulesOfLife()
     {
         // Determine if that cell should live or die based on it's neighbours.
@@ -79,37 +88,50 @@ public class GameBoard : MonoBehaviour
                 {
                     futureGen[i][j].SetCellColor(Color.yellow);
 
-                    if (board[i][j].liveNeighbourCount < 2)
+                    if (board[i][j].GetLiveNeighbourCount() < 2)
                     {
                         //UnderPopulation
-                        futureGen[i][j].SetCellState(CellState.Dead);
-                        futureGen[i][j].SetCellColor(Color.gray);
+                        KillFutureCell(i, j);
                     }
-                    else if (board[i][j].liveNeighbourCount == 2 || board[i][j].liveNeighbourCount == 3)
+                    else if (board[i][j].GetLiveNeighbourCount() == 2 || board[i][j].GetLiveNeighbourCount() == 3)
                     {
                         //Lives on
-                        futureGen[i][j].SetCellState(CellState.Alive);
-                        futureGen[i][j].SetCellColor(Color.yellow);
+                        FutureCellLives(i, j);
                     }
-                    else if (board[i][j].liveNeighbourCount > 3)
+                    else if (board[i][j].GetLiveNeighbourCount() > 3)
                     {
                         //OverPopulation
-                        futureGen[i][j].SetCellState(CellState.Dead);
-                        futureGen[i][j].SetCellColor(Color.gray);
+                        KillFutureCell(i, j);
                     }
                 }
                 else
                 {
-                    if (board[i][j].liveNeighbourCount == 3)
+                    if (board[i][j].GetLiveNeighbourCount() == 3)
                     {
                         //Reproduction.
-                        futureGen[i][j].SetCellState(CellState.Alive);
-                        futureGen[i][j].SetCellColor(Color.yellow);
+                        FutureCellLives(i, j);
                     }
                 }
             }
         }
     }
+    
+    //Turn future cells "on"
+    private void FutureCellLives(int i, int j)
+    {
+        futureGen[i][j].SetCellState(CellState.Alive);
+        futureGen[i][j].SetCellColor(Color.yellow);
+    }
+
+    //Turn future cells "off"
+    private void KillFutureCell(int i, int j)
+    {
+        futureGen[i][j].SetCellState(CellState.Dead);
+        futureGen[i][j].SetCellColor(Color.gray);
+    }
+
+
+    //Count how many live/dead neighbours each cell has.
     private void CountNeighbours()
     {
         for (int i = 0; i < board.Length; i++)
@@ -118,31 +140,32 @@ public class GameBoard : MonoBehaviour
             for (int j = 0; j < board[i].Length; j++)
             {
                 //loop through each cell in the cell array.
-                for (int k = 0; k < board[i][j].cellNeighbors.Length; k++)
+                for (int k = 0; k < board[i][j].GetCellNeighbours().Length; k++)
                 {
                     
                     //loop through each cell neighbour array within each cell.
-                    if (board[i][j].cellNeighbors[k] != null)
+                    if (board[i][j].GetCellNeighbours()[k] != null)
                     {
-                        if (board[i][j].cellNeighbors[k].GetCellState() == CellState.Dead)
+                        if (board[i][j].GetCellNeighbours()[k].GetCellState() == CellState.Dead)
                         {
-                            board[i][j].deadNeighbourCount++;
+                            board[i][j].AddDeadNeighbourCount(1);
                         }
-                        else if(board[i][j].cellNeighbors[k].GetCellState() == CellState.Alive)
+                        else if(board[i][j].GetCellNeighbours()[k].GetCellState() == CellState.Alive)
                         {
-                            board[i][j].liveNeighbourCount++;
+                            board[i][j].AddLiveNeighbourCount(1);
                         }
                     }
                     else
                     {
-                        //Edgecase
-                        board[i][j].deadNeighbourCount++;
+                        //Handling the edge case by making edges dead cells.
+                        board[i][j].AddDeadNeighbourCount(1);
                     }
                 }
             }
         }
     }
 
+    //Fill our neighbour array with cell neighbours.
     private void FillCellNeighbors()
     {
         for (int i = 0; i < board.Length; i++)
@@ -150,47 +173,49 @@ public class GameBoard : MonoBehaviour
             for (int j = 0; j < board[i].Length; j++)
             {
                 //Create neighbors array.
-                board[i][j].cellNeighbors = new Cell[8];
+                board[i][j].SetCellNeighbours(new Cell[8]);
 
-                //fill neighbors array with all neighbors.
+                //Fill neighbors array with all neighbors.
                 if (i - 1 >= 0 && j + 1 <= arraySize - 1)
                 {
-                    board[i][j].cellNeighbors[0] = board[i - 1][j + 1]; //top left
+                    board[i][j].GetCellNeighbours()[0] = board[i - 1][j + 1]; //top left
                 }
                 if (j + 1 <= arraySize - 1)
                 {
-                    board[i][j].cellNeighbors[1] = board[i][j + 1];     //top
+                    board[i][j].GetCellNeighbours()[1] = board[i][j + 1];     //top
                 }
                 if (i + 1 <= arraySize - 1 && j + 1 <= arraySize - 1)
                 {
-                    board[i][j].cellNeighbors[2] = board[i + 1][j + 1]; //top right
+                    board[i][j].GetCellNeighbours()[2] = board[i + 1][j + 1]; //top right
                 }
                 if (i + 1 <= arraySize - 1)
                 {
-                    board[i][j].cellNeighbors[3] = board[i + 1][j];     //right
+                    board[i][j].GetCellNeighbours()[3] = board[i + 1][j];     //right
                 }
                 if (i + 1 <= arraySize - 1 && j - 1 >= 0)
                 {
-                    board[i][j].cellNeighbors[4] = board[i + 1][j - 1]; //bottom right
+                    board[i][j].GetCellNeighbours()[4] = board[i + 1][j - 1]; //bottom right
                 }
                 if (j - 1 >= 0)
                 {
-                    board[i][j].cellNeighbors[5] = board[i][j - 1];     //bottom
+                    board[i][j].GetCellNeighbours()[5] = board[i][j - 1];     //bottom
                 }
                 if (i - 1 >= 0 && j - 1 >= 0)
                 {
-                    board[i][j].cellNeighbors[6] = board[i - 1][j - 1]; //bottom left
+                    board[i][j].GetCellNeighbours()[6] = board[i - 1][j - 1]; //bottom left
                 }
                 if (i - 1 >= 0)
                 {
-                    board[i][j].cellNeighbors[7] = board[i - 1][j];     //left
+                    board[i][j].GetCellNeighbours()[7] = board[i - 1][j];     //left
                 }
             }
         }
     }
 
+    //Starting grid of all dead cells.
     public void CreateInitialGrid()
     {
+        
         generationCounter = 1;
         generationNumber.text = generationCounter.ToString();
         // Create grid of cells.
@@ -201,13 +226,14 @@ public class GameBoard : MonoBehaviour
             for (int j = 0; j < board[i].Length; j++)
             {
                 board[i][j] = Instantiate<Cell>(cell, new Vector3(i, j, 0f), Quaternion.identity);
-                board[i][j].SetCellState(CellState.Dead);
-                board[i][j].SetCellColor(Color.gray);
-              
+                CurrentCellDies(i, j);
+
             }
         }
     }
 
+   
+    //UnityEvent callback function that creates a random grid of alive/dead cells.
     public void RandomGrid()
     {
         generationCounter = 1;
@@ -223,18 +249,30 @@ public class GameBoard : MonoBehaviour
                 int decider = UnityEngine.Random.Range(0, 2);
                 if (decider == 0)
                 {
-                    board[i][j].SetCellState(CellState.Alive);
-                    board[i][j].SetCellColor(Color.yellow);
+                    CurrentCellLives(i, j);
                 }
                 else
                 {
-                    board[i][j].SetCellState(CellState.Dead);
-                    board[i][j].SetCellColor(Color.gray);
+                    CurrentCellDies(i, j);
                 }
             }
         }
     }
 
+    //Turning "off" a current generation cell.
+    private void CurrentCellDies(int i, int j)
+    {
+        board[i][j].SetCellState(CellState.Dead);
+        board[i][j].SetCellColor(Color.gray);
+    }
+    //Turning "on" a current generation cell.
+    private void CurrentCellLives(int i, int j)
+    {
+        board[i][j].SetCellState(CellState.Alive);
+        board[i][j].SetCellColor(Color.yellow);
+    }
+
+    //Set the camera to follow the size of the array.
     private void SetupCamera()
     {
         cam.orthographicSize = arraySize / 2f;
@@ -243,37 +281,67 @@ public class GameBoard : MonoBehaviour
         cam.transform.position = newCameraPosition;
     }
 
+    //Only update the game board if the game isnt paused.
     private void Update()
     {
-        
+        MouseClicked();
 
-        if (Input.GetMouseButtonDown(0))
-        {
-            Vector3 mouseToWorldPoint = cam.ScreenToWorldPoint(Input.mousePosition);
-            if(Mathf.Abs(Mathf.RoundToInt(mouseToWorldPoint.x)) < arraySize && Mathf.Abs(Mathf.RoundToInt(mouseToWorldPoint.x)) >= 0 && Mathf.Abs(Mathf.RoundToInt(mouseToWorldPoint.y)) < arraySize && Mathf.Abs(Mathf.RoundToInt(mouseToWorldPoint.y)) >= 0)
-            {
-               
-                if (board[Mathf.Abs(Mathf.RoundToInt(mouseToWorldPoint.x))][Mathf.Abs(Mathf.RoundToInt(mouseToWorldPoint.y))].cellState == CellState.Alive)
-                {
-                   
-                    futureGen[Mathf.Abs(Mathf.RoundToInt(mouseToWorldPoint.x))][Mathf.Abs(Mathf.RoundToInt(mouseToWorldPoint.y))].cellState = CellState.Dead;
-                    futureGen[Mathf.Abs(Mathf.RoundToInt(mouseToWorldPoint.x))][Mathf.Abs(Mathf.RoundToInt(mouseToWorldPoint.y))].SetCellColor(Color.gray);
-                }
-                else
-                {
-                    futureGen[Mathf.Abs(Mathf.RoundToInt(mouseToWorldPoint.x))][Mathf.Abs(Mathf.RoundToInt(mouseToWorldPoint.y))].cellState = CellState.Alive;
-                    futureGen[Mathf.Max(Mathf.RoundToInt(mouseToWorldPoint.x), 0)][Mathf.Abs(Mathf.RoundToInt(mouseToWorldPoint.y))].SetCellColor(Color.yellow);
-                }
-            }
-        }
-
-        if(paused) return;
+        if (paused) return;
         GameBoardUpdate();
     }
 
+    //Flips the state of a cell when the user clicks the left mouse button over the cell.
+    private void MouseClicked()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mouseToWorldPoint = cam.ScreenToWorldPoint(Input.mousePosition);
+            if (MouseWithinTheBoundsOfArray(mouseToWorldPoint))
+            {
+
+                if (MouseClickedOnLivingCell(mouseToWorldPoint))
+                {
+                    ClickedOnCellDies(mouseToWorldPoint);
+                }
+                else
+                {
+                    ClickedOnCellLives(mouseToWorldPoint);
+                }
+            }
+        }
+    }
+
+    //If we click on this cell, it lives.
+    private void ClickedOnCellLives(Vector3 mouseToWorldPoint)
+    {
+        futureGen[Mathf.Abs(Mathf.RoundToInt(mouseToWorldPoint.x))][Mathf.Abs(Mathf.RoundToInt(mouseToWorldPoint.y))].SetCellState(CellState.Alive);
+        futureGen[Mathf.Abs(Mathf.RoundToInt(mouseToWorldPoint.x))][Mathf.Abs(Mathf.RoundToInt(mouseToWorldPoint.y))].SetCellColor(Color.yellow);
+    }
+
+    //If we click on this cell, it dies.
+    private void ClickedOnCellDies(Vector3 mouseToWorldPoint)
+    {
+        futureGen[Mathf.Abs(Mathf.RoundToInt(mouseToWorldPoint.x))][Mathf.Abs(Mathf.RoundToInt(mouseToWorldPoint.y))].SetCellState(CellState.Dead);
+        futureGen[Mathf.Abs(Mathf.RoundToInt(mouseToWorldPoint.x))][Mathf.Abs(Mathf.RoundToInt(mouseToWorldPoint.y))].SetCellColor(Color.gray);
+    }
+    //Determine if the cell we click on is alive.
+    private bool MouseClickedOnLivingCell(Vector3 mouseToWorldPoint)
+    {
+        return board[Mathf.Abs(Mathf.RoundToInt(mouseToWorldPoint.x))][Mathf.Abs(Mathf.RoundToInt(mouseToWorldPoint.y))].GetCellState() == CellState.Alive;
+    }
+
+    //Determine if the cell we clicked on is within the bounds of the array.
+    private bool MouseWithinTheBoundsOfArray(Vector3 mouseToWorldPoint)
+    {
+        return Mathf.Abs(Mathf.RoundToInt(mouseToWorldPoint.x)) < arraySize &&
+            Mathf.Abs(Mathf.RoundToInt(mouseToWorldPoint.x)) >= 0 &&
+            Mathf.Abs(Mathf.RoundToInt(mouseToWorldPoint.y)) < arraySize &&
+            Mathf.Abs(Mathf.RoundToInt(mouseToWorldPoint.y)) >= 0;
+    }
+
+    //Update the game board based on the future generation calculations.
     public void GameBoardUpdate()
     {
-        //have this update the game board every second. Not every frame.
         CreateNextGeneration();
         FillCellNeighbors();
         CountNeighbours();
@@ -281,18 +349,21 @@ public class GameBoard : MonoBehaviour
         UpdateStateOfBoard();
     }
 
+    //UnityEvent callback function that pauses the game
     public void PauseGame()
     {
         Time.timeScale = 0;
         paused = true;
     }
 
+    //UnityEvent callback function that resumes the game
     public void ResumeGame()
     {
         Time.timeScale = 1;
         paused = false;
     }
 
+    //UnityEvent callback function that steps through one generation.
     public void Step()
     {
         paused = true;
